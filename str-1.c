678a80921bc891d7eeb427607650b28e929921fd
Amit <joe.amit@yahoo.com>
2022-11-14 23:15:47 +0530

    This finds the length of string

--- str-1.c
+++ str-1.c
@@ -2,14 +2,13 @@
 1. Write a program to calculate the length of the string.
 (without using built-in method)
 */
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int i;
     char str[200];
     printf("Enter any Word to gets its length: ");
     gets(str);
-    for ( i = 0;str[i]; i++);
-    printf("%d",i);
-    
+    for (i = 0; str[i]; i++);
+    printf("%d", i);
 }
